149d71f2338df619df5a51eb07bbb953431432ed
andre <andre@andres-iMac.local>
2023-08-06 21:45:38 +0100

    add for and o2 comments

--- queries/o2/highlights.scm
+++ queries/o2/highlights.scm
@@ -4,6 +4,8 @@
 ((comment_c) @comment.documentation
   (#lua-match? @comment.documentation "^/[*][*][^*].*[*]/$"))
 
+(comment_o2) @comment @spell
+
 (primitive_type
 [
  "double"
@@ -15,3 +17,13 @@
 (string_literal) @string
 
 (include "include") @include
+; "include" @include
+
+(for_stamente
+[
+ "for"
+ "to"
+ "do"
+ "end for"
+ ]
+ @repeat)
