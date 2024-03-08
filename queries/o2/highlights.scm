f451179b4bf146e949f507ee4f8c7a384becda52
aReigoto <18371208+aReigoto@users.noreply.github.com>
2024-03-08 13:50:05 +0000

    comment all highlights

--- queries/o2/highlights.scm
+++ queries/o2/highlights.scm
@@ -1,30 +1,32 @@
-; C like comments
-(comment_c) @comment @spell
-
-((comment_c) @comment.documentation
-  (#lua-match? @comment.documentation "^/[*][*][^*].*[*]/$"))
-
-(comment_o2) @comment @spell
-
-(primitive_type
-[
- "double"
- "string"
-] @type)
-
-(number_literal) @number
-
-(string_literal) @string
-
-(include_statment "include") @include
-; "include" @include
-
-(for_statement
-[
- "for"
- "to"
- "do"
- "end"
- "for"
- ]
- @repeat)
+// 
+// ; C like comments
+// (comment_c) @comment @spell
+// 
+// ((comment_c) @comment.documentation
+//   (#lua-match? @comment.documentation "^/[*][*][^*].*[*]/$"))
+// 
+// (comment_o2) @comment @spell
+// 
+// (primitive_type
+// [
+//  "double"
+//  "string"
+// ] @type)
+// 
+// (number_literal) @number
+// 
+// (string_literal) @string
+// 
+// (include_statment "include") @include
+// ; "include" @include
+// 
+// (for_statement
+// [
+//  "for"
+//  "to"
+//  "do"
+//  "end"
+//  "for"
+//  ]
+//  @repeat)
+// 
